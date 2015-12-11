52ef27b13a451e183d3f15d25081d26eed894075
Kelly Dwan <kelly.dwan@gmail.com>
2015-12-11 18:50:59 -0500

    Break apart the giant setup function

--- js/components/controller.jsx
+++ js/components/controller.jsx
@@ -9,11 +9,68 @@ import Index from './posts';
 import SinglePost from './post';
 import Term from './term';
 
-// Private vars
-var _currentSlug, _currentType, _currentPage;
+/**
+ * The current slug, can be for single posts, pages, or term archives.
+ * @type {array}
+ * @protected
+ */
+var _currentSlug;
+
+/**
+ * The current page type: post, page, category, post_tag
+ * @type {array}
+ * @protected
+ */
+var _currentType;
+
+/**
+ * The current pagination value for an archive (defaults to 1 on single views)
+ * @type {array}
+ * @protected
+ */
+var _currentPage;
+
+/**
+ * Set the classes on the <body> tag.
+ *
+ * @param {string} type - the type of page this is
+ */
+var setBodyClass = function( type ) {
+	let bodyClass = {
+		'logged-in': ( parseInt( FoxhoundSettings.user ) !== 0 ),
+		'home': ( 'home' === type ),
+		'single': ( 'single' === type ),
+		'archive': ( 'archive' === type ),
+		'single-page': ( 'single' === type ) && ( 'page' === _currentType ),
+		'single-post': ( 'single' === type ) && ( 'post' === _currentType ),
+	};
+
+	document.body.className = classNames( bodyClass );
+}
 
 let Controller = {
-	setup: function( context, next ) {
+	setupHome: function( context, next ) {
+		_currentPage = parseInt( context.params.page ) || 1;
+
+		setBodyClass( 'home' );
+		window.scrollTo( 0, 0 );
+		next();
+	},
+
+	setupTerm: function( context, next ) {
+		var path = context.pathname;
+
+		if ( 'undefined' !== typeof context.params.term ) {
+			_currentSlug = context.params.term;
+			_currentType = path.match( /^\/category/ ) ? 'category' : 'post_tag';
+		}
+
+		setBodyClass( 'archive' );
+		window.scrollTo( 0, 0 );
+		next();
+	},
+
+	setupSingle: function( context, next ) {
 		var path = context.pathname;
 
 		if ( path.substr( -1 ) === '/' ) {
@@ -28,25 +85,8 @@ let Controller = {
 			_currentType = 'page';
 		}
 
-		if ( 'undefined' !== typeof context.params.term ) {
-			_currentSlug = context.params.term;
-			_currentType = path.match( /^\/category/ ) ? 'category' : 'post_tag';
-		}
-
-		let bodyClass = {
-			'logged-in': ( parseInt( FoxhoundSettings.user ) !== 0 ),
-			'home': ( path.length === 0 ),
-			'single': ( path.length !== 0 ) && ( ( 'page' === _currentType ) || ( 'post' === _currentType ) ),
-			'archive': ( 'category' === _currentType ) || ( 'post_tag' === _currentType ),
-			'single-page': ( 'page' === _currentType ),
-			'single-post': ( 'post' === _currentType ),
-		};
-		document.body.className = classNames( bodyClass );
-
-		_currentPage = parseInt( context.params.page ) || 1;
-
+		setBodyClass( 'single' );
 		window.scrollTo( 0, 0 );
-
 		next();
 	},
 
--- js/index.jsx
+++ js/index.jsx
@@ -15,14 +15,15 @@ import Controller from './components/controller';
 
 page.base( '/' );
 
-page( '',                          Controller.setup, Controller.navigation, Controller.posts );
-page( 'page/:page',                Controller.setup, Controller.navigation, Controller.posts );
-page( 'category/:term',            Controller.setup, Controller.navigation, Controller.termArchive );
-page( 'category/:term/page/:page', Controller.setup, Controller.navigation, Controller.termArchive );
-page( 'tag/:term',                 Controller.setup, Controller.navigation, Controller.termArchive );
-page( 'tag/:term/page/:page',      Controller.setup, Controller.navigation, Controller.termArchive );
+page( '',                          Controller.setupHome, Controller.navigation, Controller.posts );
+page( 'page/:page',                Controller.setupHome, Controller.navigation, Controller.posts );
+
+page( 'category/:term',            Controller.setupTerm, Controller.navigation, Controller.termArchive );
+page( 'category/:term/page/:page', Controller.setupTerm, Controller.navigation, Controller.termArchive );
+page( 'tag/:term',                 Controller.setupTerm, Controller.navigation, Controller.termArchive );
+page( 'tag/:term/page/:page',      Controller.setupTerm, Controller.navigation, Controller.termArchive );
 
 // Don't take over wp-admin
-page( /^(?!wp-admin).*/, Controller.setup, Controller.navigation, Controller.post );
+page( /^(?!wp-admin).*/,           Controller.setupSingle, Controller.navigation, Controller.post );
 
 page.start();
