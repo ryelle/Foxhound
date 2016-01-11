c9b5f948a6e591bb430a2b7879eeabebc5ec3865
Kelly Dwan <kelly.dwan@gmail.com>
2016-01-11 18:50:10 -0500

    Add the Search page to render search results

--- js/components/search/index.jsx
+++ js/components/search/index.jsx
@@ -1,22 +1,89 @@
 // External dependencies
 import React from 'react';
+import isEqual from 'lodash/lang/isEqual';
 import classNames from 'classnames';
 
-let SearchForm = React.createClass( {
-	getValue: function() {
-		return this.refs.input.value;
+// Internal dependencies
+import API from 'utils/api';
+import PostsStore from '../../stores/posts-store';
+import PostList from '../posts/list';
+import Pagination from '../pagination/archive';
+import SearchForm from './form';
+
+
+/**
+ * Method to retrieve state from Stores
+ */
+function getState() {
+	return {
+		data: PostsStore.getPosts(),
+		paginationLimit: PostsStore.getPaginationLimit(),
+	};
+}
+
+let Search = React.createClass( {
+
+	propTypes: {
+		page: React.PropTypes.number.isRequired,
 	},
+
+	getInitialState: function() {
+		return getState();
+	},
+
+	componentDidMount: function() {
+		PostsStore.addChangeListener( this._onChange );
+
+		let filter = {
+			s: this.getSearchValue(),
+		};
+		API.getPosts( { filter: filter, page: this.props.page } );
+	},
+
+	componentDidUpdate: function( prevProps, prevState ) {
+		if ( ! isEqual( prevProps, this.props ) ) {
+			let filter = {
+				s: this.getSearchValue(),
+			};
+			API.getPosts( { filter: filter, page: this.props.page } );
+		}
+	},
+
+	componentWillUnmount: function() {
+		PostsStore.removeChangeListener( this._onChange );
+	},
+
+	_onChange: function() {
+		this.setState( getState() );
+	},
+
+	getSearchValue: function() {
+		if ( 'undefined' !== typeof this.refs.searchForm ) {
+			return this.refs.searchForm.getValue();
+		}
+		return '';
+	},
+
+	setTitle: function() {
+		let term = this.getSearchValue();
+		document.title = `Search results for ${term} — ${FoxhoundSettings.title}`;
+	},
+
 	render: function() {
+		let posts = this.state.data;
+		this.setTitle();
+
 		return (
-			<form role="search" className="search-form">
-				<label>
-					<span className="screen-reader-text">Search for:</span>
-					<input ref='input' type="search" className="search-field" placeholder="Search …" name="s" title="Search for:" onChange={ this.props.onChange } />
-				</label>
-				<input type="submit" className="search-submit" value="Search" />
-			</form>
+			<div>
+				<SearchForm ref='searchForm' initialSearch={ this.props.term } onChange={ this.search } />
+
+				{ posts.length ?
+					<PostList posts={ posts } /> :
+					null
+				}
+			</div>
 		);
 	}
 } );
 
-export default SearchForm;
+export default Search;
