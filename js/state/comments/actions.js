0b184b01847632e512ed702f251c7ffc2b38874c
Kelly Dwan <kelly.dwan@gmail.com>
2016-06-04 14:39:33 -0400

    Add comment state

---
+++ js/state/comments/actions.js
@@ -0,0 +1,56 @@
+/* global FoxhoundSettings */
+/**
+ * Internal dependencies
+ */
+const WP = require( 'wordpress-rest-api' )( { endpoint: FoxhoundSettings.URL.root } );
+import {
+	COMMENTS_REQUEST,
+	COMMENTS_REQUEST_SUCCESS,
+	COMMENTS_REQUEST_FAILURE,
+	COMMENTS_RECEIVE,
+} from 'state/action-types';
+
+/**
+ * Returns an action object to be used in signalling that a list of comment
+ * objects have been received.
+ *
+ * @param  {Object}  comments  Comments received
+ * @return {Object}            Action object
+ */
+export function receiveComments( comments ) {
+	return {
+		type: COMMENTS_RECEIVE,
+		comments
+	};
+}
+
+/**
+ * Triggers a network request to fetch the comments for a given post.
+ *
+ * @param  {int}       postId  Post ID of post to get comments for
+ * @return {Function}          Action thunk
+ */
+export function requestComments( postId ) {
+	return ( dispatch ) => {
+		dispatch( {
+			type: COMMENTS_REQUEST,
+			postId,
+		} );
+
+		return WP.posts().comments().then( ( data ) => {
+			dispatch( receiveComments( data ) );
+			dispatch( {
+				type: COMMENTS_REQUEST_SUCCESS,
+				comments: data,
+				count: data._paging.total,
+				postId
+			} );
+		} ).catch( ( error ) => {
+			dispatch( {
+				type: COMMENTS_REQUEST_FAILURE,
+				postId,
+				error
+			} );
+		} );
+	};
+}
---
+++ js/state/comments/selectors.js
@@ -0,0 +1,55 @@
+/**
+ * Returns a comment object by its global ID.
+ *
+ * @param  {Object} state    Global state tree
+ * @param  {String} globalId Comment global ID
+ * @return {Object}          Comment object
+ */
+export function getComment( state, globalId ) {
+	return state.comments.items[ globalId ];
+}
+
+/**
+ * Returns an array of comments for a given post, or null if no comments have
+ * been received.
+ *
+ * @param  {Object}  state   Global state tree
+ * @param  {int}     postId  Post ID for the queried post
+ * @return {?Array}          Comments for the given post
+ */
+export function getCommentsForPost( state, postId ) {
+	if ( ! state.comments.results[ postId ] ) {
+		return null;
+	}
+
+	return state.comments.results[ postId ].map( ( globalId ) => {
+		return getComment( state, globalId );
+	} ).filter( Boolean );
+}
+
+/**
+ * Returns true if currently requesting comments for a post ID, or false
+ * otherwise.
+ *
+ * @param  {Object}  state   Global state tree
+ * @param  {int}     postId  Post ID for the queried post
+ * @return {Boolean}         Whether comments are being requested
+ */
+export function isRequestingCommentsForPost( state, postId ) {
+	return !! state.posts.requests[ postId ];
+}
+
+/**
+ * Returns the number of total comments available for a given post
+ *
+ * @param  {Object}  state   Global state tree
+ * @param  {int}     postId  Post ID for the queried post
+ * @return {int}             Number of comments
+ */
+export function getTotalCommentsForPost( state, postId ) {
+	if ( ! state.posts.totals[ postId ] ) {
+		return 0;
+	}
+
+	return parseInt( state.posts.totals[ postId ] );
+}
