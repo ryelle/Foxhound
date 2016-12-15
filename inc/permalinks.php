981adb407c10091b54cd12a49d41ea3626086203
Kelly Dwan <kelly.dwan@gmail.com>
2016-12-14 22:43:31 -0500

    Add a warning on theme activation that permalinks have changed. Add more information about permalinks to the contextual help.
    Fixes #51

--- inc/permalinks.php
+++ inc/permalinks.php
@@ -19,6 +19,7 @@ class Foxhound_SetPermalinks {
 		add_action( 'init', array( $this, 'change_page' ) );
 		add_action( 'after_switch_theme', array( $this, 'update_permalinks' ), 11 );
 		add_action( 'template_redirect', array( $this, 'redirect_search' ) );
+		add_action( 'admin_head-options-permalink.php', array( $this, 'add_contextual_permalink_help' ) );
 
 		// Flush permalinks after the theme is activated.
 		add_action( 'after_switch_theme', 'flush_rewrite_rules' );
@@ -34,9 +35,40 @@ class Foxhound_SetPermalinks {
 		}
 		?>
 		<div class="notice notice-warning">
-			<p><?php _e( '<b>Warning:</b> The theme you\'re using does not support customized permalinks.', 'foxhound' ); ?></p>
+			<p><?php _e( '<b>Warning:</b> The theme you\'re using does not support customized permalinks. See the Help tab above for more information.', 'foxhound' ); ?></p>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Add a warning message to the theme screen after activation.
+	 */
+	public function admin_theme_warning() {
+		?>
+		<div class="notice notice-warning">
+			<p><?php _e( 'This theme requires special URLs to display your content, so your permalinks have been updated. To undo this, switch to another theme.', 'foxhound' ); ?></p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Add a contextual help tab to the permalinks screen to explain new permalinks.
+	 */
+	public function add_contextual_permalink_help() {
+		$screen = get_current_screen();
+
+		$content = '<p>' . __( 'Foxhound is a different kind of theme, which means it needs to handle URLs differently than other themes. There is no permalink customization in Foxhound, you need to use the "Month and name" setting for posts.', 'foxhound' ) . '</p>';
+		$content .= '<p>' . __( 'Other content on your site also needs different URLs, like the following:', 'foxhound' ) . '<ul>';
+		$content .= '<li>' . __( 'Pages are now prefixed with <code>page</code>; for example, <code>%1$s/page/about/</code>', 'foxhound' ) . '</li>';
+		$content .= '<li>' . __( 'Date archives are now prefixed with <code>date</code>; for example, <code>%1$s/date/2016/12/</code>', 'foxhound' ) . '</li>';
+		$content .= '<li>' . __( 'Category and tag archives are prefixed with <code>category</code> and <code>tag</code>, which is core behavior, but this cannot be changed.', 'foxhound' ) . '</li>';
+		$content .= '</ul></p>';
+
+		$screen->add_help_tab( array(
+			'id'      => 'foxhound_permalinks_help',
+			'title'   => __( 'Foxhound Permalinks', 'foxhound' ),
+			'content' => sprintf( $content, untrailingslashit( site_url() ) ),
+		) );
 	}
 
 	/**
@@ -45,6 +77,7 @@ class Foxhound_SetPermalinks {
 	public function update_permalinks() {
 		global $wp_rewrite;
 		$wp_rewrite->set_permalink_structure( '/%year%/%monthnum%/%postname%/' );
+		add_action( 'admin_notices', array( $this, 'admin_theme_warning' ) );
 	}
 
 	/**
