727423b8b8cb9ec2ddde27c36c638b64a8a9ab4a
Kelly Dwan <kelly.dwan@gmail.com>
2016-12-30 12:01:15 -0500

    Add an author archive (#61)
    * Add query-user component
    * Add author route & initial archive components
    * Update to `page` over `paged` for API

---
+++ js/components/author/index.jsx
@@ -0,0 +1,56 @@
+/*global FoxhoundSettings */
+import React from 'react';
+import { connect } from 'react-redux';
+import DocumentMeta from 'react-document-meta';
+import BodyClass from 'react-body-class';
+
+// Internal dependencies
+import QueryUser from 'wordpress-query-user';
+import { isRequestingUser, getUserIdFromName, getUser } from 'wordpress-query-user/lib/selectors';
+import Placeholder from 'components/placeholder';
+import List from './list';
+
+const AuthorHeader = ( { userName, loading, user = {}, query = {} } ) => {
+	const meta = {
+		title: user.name + ' – ' + FoxhoundSettings.meta.title,
+	};
+
+	return (
+		<div className="card">
+			<DocumentMeta { ...meta } />
+			<BodyClass classes={ [ 'archive', 'author' ] } />
+			<QueryUser userId={ userName } />
+			{ loading ?
+				<Placeholder type="author" /> :
+				<div>
+					<header className="page-header">
+						<h1 className="page-title">Archive for { user.name }</h1>
+						{ user.description && <p>{ user.description }</p> }
+					</header>
+					<List query={ query } author={ user.slug } />
+				</div>
+			}
+		</div>
+	);
+}
+
+export default connect( ( state, ownProps ) => {
+	const userName = ownProps.params.slug;
+	const user = getUser( state, userName );
+	const userId = getUserIdFromName( state, userName );
+	const requesting = isRequestingUser( state, userName );
+
+	const query = {};
+	query.page = ownProps.params.paged || 1;
+	if ( userId ) {
+		query.author = [ userId ];
+	}
+
+	return {
+		query,
+		userName,
+		user,
+		requesting,
+		loading: requesting && ! user
+	};
+} )( AuthorHeader );
---
+++ js/components/author/list.jsx
@@ -0,0 +1,60 @@
+/*global FoxhoundSettings */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import isEqual from 'lodash/isequal';
+
+// Internal dependencies
+import QueryPosts from 'wordpress-query-posts';
+import { isRequestingPostsForQuery, getPostsForQuery, getTotalPagesForQuery } from 'wordpress-query-posts/lib/selectors';
+
+// Components
+import PostList from 'components/posts/list';
+import Pagination from 'components/pagination/archive';
+import Placeholder from 'components/placeholder';
+
+class AuthorArchive extends Component {
+	shouldComponentUpdate( nextProps ) {
+		const newQuery = ! isEqual( nextProps.query, this.props.query );
+		const newPosts = ! isEqual( nextProps.posts, this.props.posts );
+		return newQuery || newPosts;
+	}
+
+	render() {
+		const { query, posts, loading, path, page, totalPages } = this.props;
+		return (
+			<div>
+				<QueryPosts query={ query || {} } />
+				{ loading ?
+					<Placeholder type="author" /> :
+					<PostList posts={ posts } />
+				}
+
+				<Pagination
+					path={ path }
+					current={ page }
+					isFirstPage={ 1 === page }
+					isLastPage={ totalPages === page } />
+			</div>
+		);
+	}
+};
+
+export default connect( ( state, ownProps ) => {
+	const { query, author } = ownProps;
+	let path = FoxhoundSettings.URL.path || '/';
+	path += `author/${ author }/`;
+
+	// Needs to be below query setup
+	const requesting = isRequestingPostsForQuery( state, query );
+	const posts = getPostsForQuery( state, query ) || [];
+
+	return {
+		path,
+		query,
+		posts,
+		requesting,
+		loading: requesting && ! posts,
+		page: parseInt( query.page ),
+		totalPages: getTotalPagesForQuery( state, query ),
+	};
+} )( AuthorArchive );
