fe4d721b22fc250e7bfc83e6e087e14a54c3fc44
Kelly Dwan <kelly.dwan@gmail.com>
2017-01-05 18:01:23 -0500

    Use `rest_get_server`, instead of writing the same code in the theme

--- inc/load-data.php
+++ inc/load-data.php
@@ -61,21 +61,14 @@ class Foxhound_LoadData {
 
 		$posts = $GLOBALS['wp_query']->posts;
 
-		global $wp_rest_server;
-
-		if ( empty( $wp_rest_server ) ) {
-			$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
-			$wp_rest_server       = new $wp_rest_server_class;
-			do_action( 'rest_api_init' );
-		}
-
+		$rest_server        = rest_get_server();
 		$data               = array();
 		$request            = new \WP_REST_Request();
 		$request['context'] = 'view';
 
 		foreach ( (array) $posts as $post ) {
 			$controller = new \WP_REST_Posts_Controller( $post->post_type );
-			$data[]     = $wp_rest_server->response_to_data( $controller->prepare_item_for_response( $post, $request ), true );
+			$data[]     = $rest_server->response_to_data( $controller->prepare_item_for_response( $post, $request ), true );
 		}
 
 		return $data;
