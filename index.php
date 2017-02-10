f23678cf8e182c501939ffa61e42ee3f1b200352
Kelly Dwan <kelly.dwan@gmail.com>
2017-02-09 19:13:21 -0500

    Treat the entire #main as one item, when anything changes in the content — this prevents VO from jumping down to read “Leave a Reply” or comments when the comments load

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main" aria-live="assertive" tabindex="-1"></main><!-- #main -->
+		<main id="main" class="site-main" role="main" aria-live="assertive" aria-atomic="true" tabindex="-1"></main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
