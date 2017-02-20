cf9e1e1c94f4265a27bdac4c030dde582146cc9a
Kelly Dwan <kelly.dwan@gmail.com>
2017-02-20 12:37:34 -0500

    Add documentation

--- js/utils/a11y.js
+++ js/utils/a11y.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+// FocusHandler is a component which adds focus to a given element each time the page renders. This ensures
+// the keyboard focus is in a predictable location after page load.
 const FocusHandler = React.createClass( {
 	componentDidUpdate( prevProps ) {
 		const { routerProps, elementId } = this.props;
@@ -20,6 +22,13 @@ const FocusHandler = React.createClass( {
 	}
 } );
 
+/**
+ * Create a router middleware, which wraps the `RouterContext` with the FocusHandler. FocusHandler in turn
+ * sets focus on a specific element when a new page is rendered.
+ *
+ * @param  {string}  elementId  The element which should receive focus after render
+ * @return {object}  An object with `renderRouterContext` defined, to be passed back to `applyRouterMiddleware`
+ */
 export function keyboardFocusReset( elementId ) {
 	return {
 		renderRouterContext: ( child, props ) => (
@@ -33,6 +42,11 @@ export function keyboardFocusReset( elementId ) {
 	};
 };
 
+/**
+ * Set the focus to an element, based on a clicked link.
+ *
+ * @param  {HTMLElement}  clickedEl  The link element that was clicked, the href should be a reference to an ID on the page.
+ */
 export function skipLink( clickedEl ) {
 	const attributes = clickedEl.attributes;
 	const id = attributes.getNamedItem( 'href' ).value.substring( 1 );
@@ -41,6 +55,7 @@ export function skipLink( clickedEl ) {
 	}
 
 	const element = document.getElementById( id );
+	// Make the element focusable
 	if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
 		element.tabIndex = -1;
 	}
@@ -48,6 +63,11 @@ export function skipLink( clickedEl ) {
 	element.focus();
 }
 
+/**
+ * Toggle a `focus` class on navigation items as they're tabbed through.
+ *
+ * @param  {FocusEvent}  event  The blur or focus event on each link item.
+ */
 export function toggleFocus( event ) {
 	var self = event.target;
 
