2e43be40f98e4bb7e486465bb7b4c7dd6191caaf
Kelly Dwan <kelly.dwan@gmail.com>
2017-03-16 22:11:49 -0400

    Add a screen reader label for the footer widgets

--- sidebar.php
+++ sidebar.php
@@ -11,5 +11,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area sidebar-widgets" role="complementary">
+	<h2 class="screen-reader-text"><?php _e( 'Sidebar', 'foxhound' ); ?></h2>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
