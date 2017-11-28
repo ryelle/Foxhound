5ae7339c867ebf0974b4c74b666360e014e60317
Kelly Dwan <ryelle@users.noreply.github.com>
2017-11-28 11:09:11 -0500

    Add redux middleware to intercept location changes & fire the jetpack load event

--- js/state.js
+++ js/state.js
@@ -3,7 +3,7 @@
  */
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { routerReducer } from 'react-router-redux';
+import { routerReducer, LOCATION_CHANGE } from 'react-router-redux';
 import posts from 'wordpress-query-posts/lib/state';
 import pages from 'wordpress-query-page/lib/state';
 import terms from 'wordpress-query-term/lib/state';
@@ -14,7 +14,16 @@ import users from 'wordpress-query-user/lib/state';
 
 const reducer = combineReducers( { posts, pages, terms, comments, menu, media, users, routing: routerReducer } );
 
-const middleware = [ thunkMiddleware ];
+// Add the event Jetpack listens for to initialize various JS features on posts.
+const emitJetpackEvent = () => next => action => {
+	if ( LOCATION_CHANGE === action.type ) {
+		jQuery( document.body ).trigger( 'post-load' );
+	}
+
+	return next( action );
+};
+
+const middleware = [ thunkMiddleware, emitJetpackEvent ];
 
 let createStoreWithMiddleware = applyMiddleware.apply( null, middleware );
 
